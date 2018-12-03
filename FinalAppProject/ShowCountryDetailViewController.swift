1a97379529ca92c923739ee94d5fd59a1c0047fa
Paige Carey <peace.paigey@gmail.com>
2018-12-02 20:49:28 -0500

    reviews for each country fixed

---
+++ FinalAppProject/ShowCountryDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ShowCountryDetailViewController.swift
+//  FinalAppProject
+//
+//  Created by Paige Carey on 11/27/18.
+//  Copyright © 2018 Paige Carey. All rights reserved.
+//
+
+import UIKit
+
+class CountryDetailViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var flagImage: UIImageView!
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var currencyNameLabel: UILabel!
+    @IBOutlet weak var currencySymbolLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
